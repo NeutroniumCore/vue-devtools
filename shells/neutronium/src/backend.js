32d684500829b7bff4b9d2c42efc92af473f79a8
desmaisons_david <desmaisons_david@hotmail.com>
2017-03-09 00:21:27 -0300

    improve communication

--- shells/neutronium/src/backend.js
+++ shells/neutronium/src/backend.js
@@ -14,7 +14,7 @@ const bridge = new Bridge({
   send (data) {
     console.log('backend -> devtools', data)
     const dataToSend = {data, type:'data'}
-    window.__listener__.postMessage('debug', CircularJson.stringify(data))
+    window.__listener__.postMessage('debug', CircularJson.stringify(dataToSend))
   }
 })
 
